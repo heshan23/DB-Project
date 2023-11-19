9c8b99948d8d673b661192d9dc5f5e0e0da23b66
Srchycz <2982296464@qq.com>
2023-11-19 22:35:07 +0800

    前后端合并

---
+++ backend/apps/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from apps.views.user import Account as UA
+
+urlpatterns = [
+    # 用户账号
+    path('UserSignUp/', UA.UserSignUp.as_view()),
+    path('UserSignIn/', UA.Login.as_view()),
+    path('YourAccountMessage/', UA.YourAccountMessage.as_view()),
+    path('TryNewReg/', UA.TryAutoReg.as_view())
+    # 管理员功能
+
+]
