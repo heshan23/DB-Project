c8edd188a6932ca433072b06ff045921ff09ea17
Srchycz <2982296464@qq.com>
2023-11-22 16:28:38 +0800

    完善图片上传部分

--- frontend/src/pages/post/image.vue
+++ frontend/src/pages/post/image.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="clearfix">
         <a-upload action="https://localhost:8081" list-type="picture-card" :file-list="fileList" @preview="handlePreview"
-            @change="handleChange">
+            @change="handleChange" :customRequest="customRequest">
             <div v-if="fileList.length < 8">
                 <a-icon type="plus" />
                 <div class="ant-upload-text">
@@ -14,7 +14,9 @@
         </a-modal>
     </div>
 </template>
+
 <script>
+import uploadImage from '@/services/user'
 function getBase64(file) {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -34,6 +36,20 @@ export default {
         };
     },
     methods: {
+        customRequest(data) {
+            const formData = new FormData();
+            formData.append('file', data.file);
+            uploadImage(formData).then((res) => {
+                this.$message.success(res.data.reason)
+                /**
+                 * 使用 res.data.url 获取图片地址
+                 * 使用 res.data.id 获取图片id (用作查询图片的 url)
+                 */
+            }
+            ).catch((err) => {
+                this.$message.error(err.response.code)
+            })
+        },
         handleCancel() {
             this.previewVisible = false;
         },
