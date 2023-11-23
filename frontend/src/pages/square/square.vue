a63baa22a2e28488d1ca9df10b0a5491b29c0533
何山“ <21373499@buaa.edu.cn>
2023-11-23 17:12:52 +0800

    check

--- frontend/src/pages/square/square.vue
+++ frontend/src/pages/square/square.vue
@@ -2,7 +2,7 @@
     <div class="card">
         <a-card hoverable style="width: 300px;" v-for="(item, key) in contents " :key=key>
             <img slot="cover" alt="example" :src="item.picture" @click="onclick" />
-            <a-card-meta :title=item.title :description=writer @click="onclick">
+            <a-card-meta :title=item.title :description=item.writer @click="onclick">
                 <a-avatar slot="avatar" :src=item.avatar />
             </a-card-meta>
             <template v-for="{ type, text } in item.actions" slot="actions">
@@ -20,9 +20,31 @@ export default {
         return {
             contents: [
                 {
-                    title: '项目介绍',
+                    title: '旅途风光',
                     avatar: require('@/assets/img/avatar.jpg'),
-                    picture: require('@/assets/img/logo.png'),
+                    picture: require('@/assets/img/test1.png'),
+                    writer: 'heshan',
+                    actions: [
+                        { type: 'like-o', text: '2' },
+                        { type: 'star-o', text: '23' },
+                        { type: 'message', text: '234' },
+                    ],
+                },
+                {
+                    title: '摄影技巧',
+                    avatar: require('@/assets/img/avatar.jpg'),
+                    picture: require('@/assets/img/test2.png'),
+                    writer: 'heshan',
+                    actions: [
+                        { type: 'like-o', text: '156' },
+                        { type: 'star-o', text: '1990' },
+                        { type: 'message', text: '21' },
+                    ],
+                },
+                {
+                    title: '奇山峻岭',
+                    avatar: require('@/assets/img/avatar.jpg'),
+                    picture: require('@/assets/img/test3.png'),
                     writer: 'heshan',
                     actions: [
                         { type: 'like-o', text: '156' },
@@ -31,14 +53,14 @@ export default {
                     ],
                 },
                 {
-                    title: '项目介绍',
+                    title: '璀璨星空',
                     avatar: require('@/assets/img/avatar.jpg'),
-                    picture: require('@/assets/img/logo.png'),
+                    picture: require('@/assets/img/test4.png'),
                     writer: 'heshan',
                     actions: [
-                        { type: 'like-o', text: '156' },
-                        { type: 'star-o', text: '156' },
-                        { type: 'message', text: '2' },
+                        { type: 'like-o', text: '1256' },
+                        { type: 'star-o', text: '16' },
+                        { type: 'message', text: '26' },
                     ],
                 },
                 {
@@ -47,33 +69,11 @@ export default {
                     picture: require('@/assets/img/logo.png'),
                     writer: 'heshan',
                     actions: [
-                        { type: 'like-o', text: '156' },
-                        { type: 'star-o', text: '156' },
-                        { type: 'message', text: '2' },
+                        { type: 'like-o', text: '18' },
+                        { type: 'star-o', text: '120' },
+                        { type: 'message', text: '1283' },
                     ],
                 },
-                {
-                    title: '项目介绍',
-                    avatar: require('@/assets/img/avatar.jpg'),
-                    picture: require('@/assets/img/logo.png'),
-                    writer: 'heshan',
-                    actions: [
-                        { type: 'like-o', text: '156' },
-                        { type: 'star-o', text: '156' },
-                        { type: 'message', text: '2' },
-                    ],
-                },
-                {
-                    title: '项目介绍',
-                    avatar: require('@/assets/img/avatar.jpg'),
-                    picture: require('@/assets/img/logo.png'),
-                    writer: 'heshan',
-                    actions: [
-                        { type: 'like-o', text: '156' },
-                        { type: 'star-o', text: '156' },
-                        { type: 'message', text: '2' },
-                    ],
-                }
             ],
 
         }
