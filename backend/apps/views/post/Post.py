8ffb87cd8c373c82ddf699669a8d04633415738c
Srchycz <2982296464@qq.com>
2023-12-12 11:04:10 +0800

    连通article 修正post.py中返回数据格式不正确的问题 修正square获取数据时机的问题

--- backend/apps/views/post/Post.py
+++ backend/apps/views/post/Post.py
@@ -6,56 +6,46 @@ from django.utils import timezone
 from apps.views.user.Account import *
 from apps.views.user.Images import image_url
 
-default_image = image_url + '/media/images/default_background1.jpg'
+default_image = image_url + "/media/images/default_background1.jpg"
 
-default_images = [image_url + '/media/images/default_background1.jpg',
-                  image_url + '/media/images/default_background2.jpg',
-                  image_url + '/media/images/default_background3.jpg',
-                  image_url + '/media/images/default_background4.jpg']
+default_images = [
+    image_url + "/media/images/default_background1.jpg",
+    image_url + "/media/images/default_background2.jpg",
+    image_url + "/media/images/default_background3.jpg",
+    image_url + "/media/images/default_background4.jpg",
+]
 
 
 class NewPost(APIView):
     def post(self, request):
         try:
-            username = request.data['user_name']
-            title = request.data['title']
-            content = request.data['content']
-            board_name = request.data['board']
-            tags = request.data['tags']
-            image_ids = request.data['image_ids']
+            username = request.data["user_name"]
+            title = request.data["title"]
+            content = request.data["content"]
+            board_name = request.data["board"]
+            tags = request.data["tags"]
+            image_ids = request.data["image_ids"]
         except KeyError:
             return Response(
-                {"reason": "keyError,请检查发送的信息是否有user_name,title,content"},
-                status=422)
+                {"reason": "keyError,请检查发送的信息是否有user_name,title,content"}, status=422
+            )
         try:
             user = User.objects.get(user_name=username)
             post = Post.objects.create(
-                user=user,
-                title=title,
-                content=content,
-                create_date=timezone.now()
+                user=user, title=title, content=content, create_date=timezone.now()
             )
             post.save()
         except User.DoesNotExist:
-            return Response(
-                {"reason": "错误，用户名无效!!!"},
-                status=500
-            )
+            return Response({"reason": "错误，用户名无效!!!"}, status=500)
         try:
             for iid in image_ids:
                 img = Image.objects.get(id=iid)
-                PostImage.objects.create(
-                    post_id=post.id,
-                    img=img
-                )
+                PostImage.objects.create(post_id=post.id, img=img)
         except Image.DoesNotExist:
             return Response({"reason": "图片不存在"}, status=404)
         board = Board.objects.get(name=board_name)
         try:
-            board_post = BoardPost.objects.create(
-                post=post,
-                board=board
-            )
+            board_post = BoardPost.objects.create(post=post, board=board)
             board_post.save()
         except Exception as e:
             print(e)
@@ -65,19 +55,12 @@ class NewPost(APIView):
                 try:
                     real_tag = Tag.objects.get(name=tag)
                 except Tag.DoesNotExist:
-                    real_tag = Tag.objects.create(
-                        name=tag
-                    )
-                TagPost.objects.create(
-                    tag=real_tag,
-                    post=post
-                )
+                    real_tag = Tag.objects.create(name=tag)
+                TagPost.objects.create(tag=real_tag, post=post)
         except Exception as e:
             print(e)
             return Response({"reason": "tag-未知错误，请联系管理员"}, status=500)
-        return Response({
-            "reason": "创建帖子成功"
-        }, status=200)
+        return Response({"reason": "创建帖子成功"}, status=200)
 
 
 # 这种查询应该是获取的简要信息，比如点赞数，评论数，然后前端选择性展示文章内容
@@ -85,7 +68,7 @@ class NewPost(APIView):
 class PostGet(APIView):
     def get(self, request):
         try:
-            post_id = request.GET['post_id']
+            post_id = request.GET["post_id"]
             print(request.GET)
         except KeyError:
             return Response({"reason": "格式错误，检查是否有post_id"})
@@ -115,40 +98,52 @@ class PostGet(APIView):
                 append = ""
                 if comment.res_comment != -1:
                     res = Comment.objects.get(id=comment.res_comment)
-                    append = (append + "@" + str(res.user.user_name) + " " +
-                              str(res.content[0:min(10, len(res.content))]))
+                    append = (
+                        append
+                        + "@"
+                        + str(res.user.user_name)
+                        + " "
+                        + str(res.content[0 : min(10, len(res.content))])
+                    )
 
                 if comment.user.avatar is None:
                     coa = default_avatar_url
                 else:
                     coa = image_url + comment.user.avatar.img.url
-                comments.append({
-                    "comment_id": comment.id,
-                    "user_name": comment.user.user_name,
-                    "create_date": comment.create_date.strftime("%Y-%m-%d %H:%I:%S"),
-                    "content": comment.content,
-                    "reply": str(append),
-                    "avatar": coa
-                })
+                comments.append(
+                    {
+                        "comment_id": comment.id,
+                        "user_name": comment.user.user_name,
+                        "create_date": comment.create_date.strftime(
+                            "%Y-%m-%d %H:%I:%S"
+                        ),
+                        "content": comment.content,
+                        "reply": str(append),
+                        "avatar": coa,
+                    }
+                )
         except Exception as e:
             print(e)
             return Response({"reason": "服务器错误!!!"}, status=500)
-        ret = [{
+        ret = {
             "post_id": post.id,
             "user_avatar": avatar,
             "user_name": post.user.user_name,
-            'images': images,
+            "images": images,
             "title": post.title,
             "content": post.content,
             "tag": tags,
             "create_date": post.create_date.strftime("%Y-%m-%d %H:%I:%S"),
             "like_count": like_count,
-            "comments": comments
-        }]
-        return Response({
-            "reason": "查询成功",
-            "data": ret,
-        }, status=200)
+            "comments": comments,
+        }
+        return Response(
+            {
+                "reason": "查询成功",
+                "data": ret,
+            },
+            status=200,
+        )
 
 
 # 根据空不空来查询
@@ -159,47 +154,51 @@ class QueryPost(APIView):
         max_return_count = 10
         final_post = None
         good_search = True
-        username = request.GET['user_name']
+        username = request.GET["user_name"]
         try:
             # 如果是空的话，那么也会有这个键的
             user = User.objects.get(user_name=username)
-            final_post = set(Post.objects.filter(
-                user=user
-            ).values_list("id", flat=True))
+            final_post = set(
+                Post.objects.filter(user=user).values_list("id", flat=True)
+            )
         except User.DoesNotExist as e:
             print(e)
-            if username != '':
+            if username != "":
                 good_search = False
-        board_name = request.GET['board']
-        title_keyword = request.GET['title_keyword']
+        board_name = request.GET["board"]
+        title_keyword = request.GET["title_keyword"]
         try:
             if final_post is None:
-                final_post = set(Post.objects.filter(
-                    title__contains=title_keyword
-                ).values_list("id", flat=True))
+                final_post = set(
+                    Post.objects.filter(title__contains=title_keyword).values_list(
+                        "id", flat=True
+                    )
+                )
             else:
-                final_post = final_post & set(Post.objects.filter(
-                    title__contains=title_keyword
-                ).values_list("id", flat=True))
+                final_post = final_post & set(
+                    Post.objects.filter(title__contains=title_keyword).values_list(
+                        "id", flat=True
+                    )
+                )
         except Post.DoesNotExist as e:
             print(e)
-            if title_keyword != '':
+            if title_keyword != "":
                 good_search = False
         try:
             board = Board.objects.get(name=board_name)
             if final_post is None:
-                final_post = set(BoardPost.objects.filter(
-                    board=board
-                ).values_list("post", flat=True))
+                final_post = set(
+                    BoardPost.objects.filter(board=board).values_list("post", flat=True)
+                )
             else:
-                final_post = final_post & set(BoardPost.objects.filter(
-                    board=board
-                ).values_list("post", flat=True))
+                final_post = final_post & set(
+                    BoardPost.objects.filter(board=board).values_list("post", flat=True)
+                )
         except Board.DoesNotExist as e:
             print(e)
             if board_name != "":
                 good_search = False
-        tag_list = request.GET.getlist('tags[]')
+        tag_list = request.GET.getlist("tags[]")
         print("this is tags :" + str(tag_list))
         try:
             # 注意,
@@ -207,13 +206,13 @@ class QueryPost(APIView):
                 print(tag_name)
                 tag = Tag.objects.get(name=tag_name)
                 if final_post is None:
-                    final_post = set(TagPost.objects.filter(
-                        tag=tag
-                    ).values_list("post", flat=True))
+                    final_post = set(
+                        TagPost.objects.filter(tag=tag).values_list("post", flat=True)
+                    )
                 else:
-                    final_post = final_post & set(TagPost.objects.filter(
-                        tag=tag
-                    ).values_list("post", flat=True))
+                    final_post = final_post & set(
+                        TagPost.objects.filter(tag=tag).values_list("post", flat=True)
+                    )
         except Tag.DoesNotExist as e:
             print(e)
             if len(tag_list) != 0:
@@ -242,31 +241,36 @@ class QueryPost(APIView):
                 avatar = default_avatar_url
             else:
                 avatar = image_url + post.user.avatar.img.url
-            ret.append({
-                "post_id": post.id,
-                "writer": post.user.user_name,
-                "avatar": avatar,
-                "picture": image,
-                "title": post.title,
-                "content": post.content,
-                "create_date": post.create_date.strftime("%Y-%m-%d %H:%I:%S"),
-                "like_count": like_count,
-                "comment_count": comment_count,
-                "star_count": 0,
-            })
+            ret.append(
+                {
+                    "post_id": post.id,
+                    "writer": post.user.user_name,
+                    "avatar": avatar,
+                    "picture": image,
+                    "title": post.title,
+                    "content": post.content,
+                    "create_date": post.create_date.strftime("%Y-%m-%d %H:%I:%S"),
+                    "like_count": like_count,
+                    "comment_count": comment_count,
+                    "star_count": 0,
+                }
+            )
         ret = sample(ret, min(max_return_count, len(ret)))
         ret.sort(key=itemgetter("like_count"))
-        return Response({
-            "reason": "查询成功",
-            "contents": ret,
-        }, status=200)
+        return Response(
+            {
+                "reason": "查询成功",
+                "contents": ret,
+            },
+            status=200,
+        )
 
 
 class ModifyPost(APIView):
     def post(self, request, post_id):
         try:
-            new_title = request.data['title']
-            new_content = request.data['content']
+            new_title = request.data["title"]
+            new_content = request.data["content"]
         except KeyError:
             return Response({"reason": "请检查输入信息格式，是否有title和content"}, status=422)
         try:
