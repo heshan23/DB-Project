7ae8ba06f334124f78b76ab921fc8f211643fcef
Srchycz <2982296464@qq.com>
2023-12-12 18:46:30 +0800

    美化个人中心

--- frontend/src/pages/profile/profile.vue
+++ frontend/src/pages/profile/profile.vue
@@ -1,47 +1,7 @@
 <template>
-    <div class="fullPage">
-        <div class="profileTop">
-            <div class="user_info">
-                <img class="user_avatar" :src=avatar />
-                <div class="userText">
-                    <div class="userName">
-                        <span>{{ user_name }}</span>
-                    </div>
-                    <div class="signature">
-                        <span>个性签名：{{ signature }}</span>
-                    </div>
-                </div>
-            </div>
-            <div class="editButton">
-                <a-space size="small">
-                    <a-button :loading="loading" ghost>上传头像</a-button>
-                    <a-button :loading="loading" @click="edit" ghost>修改信息</a-button>
-                    <a-modal v-model="visible" title="Edit Information" :confirm-loading="confirmLoading"
-                        @ok="handleCreate">
-                        <a-form :layout="formLayout" :form="form">
-                            <a-form-item label="更改信息" :label-col="formItemLayout.labelCol"
-                                :wrapper-col="formItemLayout.wrapperCol">
-                            </a-form-item>
-                            <a-form-item label="用户名" :label-col="formItemLayout.labelCol"
-                                :wrapper-col="formItemLayout.wrapperCol">
-                                <a-input placeholder="请输入新用户名 留空则不变" v-decorator="['new_name', rules.user_name]" />
-                            </a-form-item>
-                            <a-form-item label="旧密码" :label-col="formItemLayout.labelCol"
-                                :wrapper-col="formItemLayout.wrapperCol">
-                                <a-input placeholder="请输入旧密码" v-decorator="['old_password', rules.old_password]" />
-                            </a-form-item>
-                            <a-form-item label="新密码" :label-col="formItemLayout.labelCol"
-                                :wrapper-col="formItemLayout.wrapperCol">
-                                <a-input placeholder="请输入新密码 留空则不变" v-decorator="['new_password', rules.new_password]" />
-                            </a-form-item>
-                            <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
-                            </a-form-item>
-                        </a-form>
-                    </a-modal>
-                </a-space>
-            </div>
+    <div class="fullPage" style="margin-top: 20px;">
 
-            <div class="profileStatistic">
+        <!-- <div class="profileStatistic">
                 <div class="profileStatisticItem">
                     <div class="profileStatisticItemTitle">关注</div>
                     <div class="profileStatisticItemNum">0</div>
@@ -54,6 +14,73 @@
                     <div class="profileStatisticItemTitle">发布量</div>
                     <div class="profileStatisticItemNum">0</div>
                 </div>
+            </div> -->
+        <!-- </div> -->
+        <a-row style="background-color: white; border-radius: 12px;">
+            <div class="profileTop">
+                <div class="user_info">
+                    <img class="user_avatar" :src=avatar />
+                    <div class="userText">
+                        <div class="userName">
+                            <span>{{ user_name }}</span>
+                        </div>
+                        <div class="e-mail">
+                            <span>邮箱：{{ email }}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="editButton">
+                    <a-space size="small">
+                        <a-button :loading="loading" ghost>上传头像</a-button>
+                        <a-button :loading="loading" @click="edit" ghost>修改信息</a-button>
+                        <a-modal v-model="visible" title="Edit Information" :confirm-loading="confirmLoading"
+                            @ok="handleCreate">
+                            <a-form :layout="formLayout" :form="form">
+                                <a-form-item label="更改信息" :label-col="formItemLayout.labelCol"
+                                    :wrapper-col="formItemLayout.wrapperCol">
+                                </a-form-item>
+                                <a-form-item label="用户名" :label-col="formItemLayout.labelCol"
+                                    :wrapper-col="formItemLayout.wrapperCol">
+                                    <a-input placeholder="请输入新用户名 留空则不变" v-decorator="['new_name', rules.user_name]" />
+                                </a-form-item>
+                                <a-form-item label="旧密码" :label-col="formItemLayout.labelCol"
+                                    :wrapper-col="formItemLayout.wrapperCol">
+                                    <a-input placeholder="请输入旧密码" v-decorator="['old_password', rules.old_password]" />
+                                </a-form-item>
+                                <a-form-item label="新密码" :label-col="formItemLayout.labelCol"
+                                    :wrapper-col="formItemLayout.wrapperCol">
+                                    <a-input placeholder="请输入新密码 留空则不变"
+                                        v-decorator="['new_password', rules.new_password]" />
+                                </a-form-item>
+                                <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
+                                </a-form-item>
+                            </a-form>
+                        </a-modal>
+                    </a-space>
+                </div>
+            </div>
+        </a-row>
+        <div class="myPosts" style="margin-top: 30px;">
+            <div style="background-color: white; border-radius: 12px;">
+                <div style=" margin-left:10px;margin-right: 10px;margin-bottom: 10px;">
+                    <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="contents">
+                        <a-list-item slot="renderItem" key="item.title" slot-scope="item">
+                            <template v-for="{ type, text } in item.actions" slot="actions">
+                                <span :key="type">
+                                    <a-icon :type="type" style="margin-right: 8px;" />
+                                    {{ text }}
+                                </span>
+                            </template>
+                            <img slot="extra" style="width: 200px;height: 180px;" alt="logo" :src="item.picture" />
+                            <a-list-item-meta>
+                                <a slot="title">{{ item.writer }}</a>
+                                <a-avatar slot="avatar" :src="item.avatar" />
+                            </a-list-item-meta>
+                            <h1>{{ item.title }}</h1>
+                            {{ item.content }}
+                        </a-list-item>
+                    </a-list>
+                </div>
             </div>
         </div>
 
@@ -64,15 +91,43 @@
 import { editProfile, logout } from '@/services/user'
 import { mapGetters } from 'vuex';
 // import { state } from '../../store/index'
+import { queryPost } from '../../services/user';
+
 export default {
+    created() {
+        this.$data.user_name = this.user.user_name
+        this.$data.avatar = this.user.avatar
+        this.$data.email = this.user.email
+        queryPost(undefined, this.user.user_name, undefined, undefined).then(res => {
+            console.log(res.data)
+            const dataArray = res.data.contents
+            for (let i = 0, len = dataArray.length; i < len; i++) {
+                this.$data.contents.push({
+                    title: dataArray[i].title,
+                    avatar: dataArray[i].avatar,
+                    picture: dataArray[i].picture,
+                    writer: dataArray[i].writer,
+                    actions: [
+                        { type: 'like-o', text: dataArray[i].like_count },
+                        { type: 'star-o', text: dataArray[i].star_count },
+                        { type: 'message', text: dataArray[i].comment_count },
+                    ],
+                    content: dataArray[i].content,
+                    post_id: dataArray[i].post_id
+                })
+            }
+        }).catch(err => {
+            console.log(err)
+            this.error("请求失败, 请尝试刷新页面", 1);
+        })
+    },
     data() {
-        const user_data = JSON.parse(localStorage.getItem('admin.user'))
-        console.log(user_data)
         return {
-            user_name: user_data.user_name,
+            contents: [],
+            user_name: "loading...",
             // username: this.user.user_name,
-            avatar: user_data.avatar,
-            signature: "you",
+            avatar: "",
+            email: "you",
             loading: false,
             visible: false,
             confirmLoading: false,
@@ -146,52 +201,18 @@ export default {
 }
 </script>
 
-<!-- <template>
-    <div>
-      <a-button type="primary" @click="showModal">Open Modal with async logic</a-button>
-      <a-modal v-model:open="open" title="Title" :confirm-loading="confirmLoading" @ok="handleOk">
-        <p>{{ modalText }}</p>
-      </a-modal>
-    </div>
-  </template>
-  <script lang="ts" setup>
-  import { ref } from 'vue';
-  const modalText = ref<string>('Content of the modal');
-  const open = ref<boolean>(false);
-  const confirmLoading = ref<boolean>(false);
-  
-  const showModal = () => {
-    open.value = true;
-  };
-  
-  const handleOk = () => {
-    modalText.value = 'The modal will be closed after two seconds';
-    confirmLoading.value = true;
-    setTimeout(() => {
-      open.value = false;
-      confirmLoading.value = false;
-    }, 2000);
-  };
-  </script>
-  
-   -->
 <style>
-.fullPage {
-    background-color: #adb2b9;
-}
-
 .profileTop {
-    width: 1000px;
+    /* width: 1000px; */
     height: 240px;
-    margin-top: 30px;
     padding-top: 20px;
     background-image: url("../../assets/img/background.jpg");
     /* margin-top: auto; */
-    position: absolute;
+    /* position: absolute;
     left: 50%;
     transform: translateX(-50%);
     display: flex;
-    border-radius: 5px;
+    border-radius: 5px; */
 }
 
 .user_info {
@@ -209,7 +230,7 @@ export default {
 .user_avatar {
     width: 80px;
     height: 80px;
-    background-color: #707884;
+    /* background-color: #707884; */
     overflow: hidden;
     border-radius: 50%;
 }
@@ -225,7 +246,7 @@ export default {
     color: #ffffff;
 }
 
-.signature {
+.e-mail {
     font-size: 14px;
     color: #ffffff;
 }
@@ -236,7 +257,7 @@ export default {
     margin-top: 160px;
     /* margin-left: 20px; */
     position: absolute;
-    left: 82%;
+    left: 84%;
     transform: translateX(-50%);
     display: flex;
     border-radius: 5px;
@@ -263,6 +284,16 @@ export default {
     margin-left: 20px;
     align-items: center;
     justify-content: center;
+}
+
+.myPosts {
+    /* width: 1000px; */
+    background-color: #ffffff;
+    /* position: absolute; */
+    /* left: 50%; */
+    /* transform: translateX(-50%); */
+    /* display: flex; */
+    /* border-radius: 5px; */
 }
 
 /* .Information {
